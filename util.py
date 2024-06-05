c24b4c1f42a78f0f6c2ef152aeae49940ed1ad92
rschaskos <rschaskos@icloud.com>
2024-06-05 20:35:52 -0300

    atualiza 2

--- util.py
+++ util.py
@@ -1,8 +1,4 @@
 import os
 
 def hello(msg):
-<<<<<<< HEAD
-  print(f'Seja bem-vindo(a) {msg}')
-=======
     print(f'Seja muito bem-vindo(a) {msg}! Que seja feliz!]')
->>>>>>> 6e4eb8b (atualiza util)
