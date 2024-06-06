b99dfb2e2059f638c60301f37fdb87fbe792dbba
rschaskos <rschaskos@icloud.com>
2024-06-06 19:46:08 -0300

    Altera o server

--- src/server.js
+++ src/server.js
@@ -18,6 +18,6 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
-  console.log("Boas-vindas ao AllBooks nova alteracao)
-  console.log("API disponível em http://localhost:8000")
+  console.log("Boas vindas")
+  console.log("API disponível por meio da URL http://localhost:8000")
 })
